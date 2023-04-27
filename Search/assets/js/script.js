09e18b161499d822be2414477fbc07d575c2e8ba
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-04-27 11:29:43 -0700

    fixing search

--- Search/assets/js/script.js
+++ Search/assets/js/script.js
@@ -8,17 +8,3 @@ btn.addEventListener('click', () => {
   input.focus()
 })
 
-// function search_color() {
-//     let input = document.getElementById("sb").value;
-//     input = input.toLowerCase();
-//     let x = document.getElementById("color");
-
-//     for (i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="list-item";
-//         }
-//     }
-// }
